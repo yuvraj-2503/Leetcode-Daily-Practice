d44dc029a72f9b6261b67094fcdfcf530ba3ee3b
Yuvraj Singh <singh.yuvraj1047@gmail.com>
2022-05-27 22:34:51 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (28.20%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n= nums.length;
+        int l=0, r= n-1;
+        while(l<=r){
+            int mid= l + (r-l)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target){
+                r = mid - 1;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+}
