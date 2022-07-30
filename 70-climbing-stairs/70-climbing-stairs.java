aeb19c37474e0fb26772bb4735169a0cf04f72e4
Yuvraj Singh <singh.yuvraj1047@gmail.com>
2022-07-30 20:53:31 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (7.42%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,9 +1,8 @@
 class Solution {
     public int climbStairs(int n) {
         int[] dp= new int[n+1];
-        for(int i=0; i<=n; i++){
-            dp[i] = -1;
-        }
+        dp[0] = 1;
+        dp[1] = 1;
         
         return util(n, dp);
         
@@ -12,10 +11,10 @@ class Solution {
     public static int util(int n, int[] dp){
         if(n<=1) return 1;
         
-        if(dp[(int)n] != -1){
-            return dp[(int)n];
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
         
-        return dp[(int)n] = util(n-1, dp) + util(n-2, dp);
+        return dp[n];
     }
 }
