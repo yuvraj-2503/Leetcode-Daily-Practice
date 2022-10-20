5e9dc3cc94a900c6ac517b9afe360c86d2de7998
Yuvraj Singh <singh.yuvraj1047@gmail.com>
2022-10-20 08:56:57 +0530

    Time: 15 ms (34.19%), Space: 46.8 MB (21.02%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,25 @@
+class Solution {
+    private final static TreeMap<Integer, String> map=new TreeMap<>();
+    static {
+        map.put(1000,"M");
+        map.put(900,"CM");
+        map.put(500,"D");
+        map.put(400,"CD");
+        map.put(100,"C");
+        map.put(90,"XC");
+        map.put(50,"L");
+        map.put(40,"XL");
+        map.put(10,"X");
+        map.put(9,"IX");
+        map.put(5,"V");
+        map.put(4,"IV");
+        map.put(1,"I");
+    }
+    public String intToRoman(int num) {
+        int k=map.floorKey(num);
+        if (k==num)
+            return map.get(num);
+
+        return map.get(k)+ intToRoman(num-k);
+    }
+}
