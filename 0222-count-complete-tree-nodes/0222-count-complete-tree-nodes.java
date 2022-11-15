ae4e876acdb4c6439124f61ba8bfc31097030c59
Yuvraj Singh <singh.yuvraj1047@gmail.com>
2022-11-15 12:54:12 +0530

    Time: 0 ms (100.00%), Space: 50.4 MB (18.06%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -14,19 +14,39 @@
  * }
  */
 class Solution {
-    int cnt=0;
+    
     public int countNodes(TreeNode root) {
-        inorder(root);
+        
+        if(root==null) return 0;
+        
+        int lh = leftHeight(root);
+        int rh = rightHeight(root);
+        if(lh == rh){
+            return ((2<<lh) - 1);
+        }
+        
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+    
+    int leftHeight(TreeNode root){
+        
+        int cnt=0;
+        while(root.left != null){
+            cnt++;
+            root= root.left;
+        }
         return cnt;
     }
     
-    void inorder(TreeNode root){
-        if(root == null){
-            return;
-        }
+    int rightHeight(TreeNode root){
         
-        inorder(root.left);
-        cnt++;
-        inorder(root.right);
+        int cnt=0;
+        while(root.right != null){
+            cnt++;
+            root= root.right;
+        }
+        return cnt;
     }
+    
+   
 }
