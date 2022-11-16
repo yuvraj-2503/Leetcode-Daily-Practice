9f3355fd2348982e274b031745ac2a2dcf2455a6
Yuvraj Singh <singh.yuvraj1047@gmail.com>
2022-11-16 21:19:32 +0530

    Added solution - LeetHub

--- Sum of Beauty of All Substrings - GFG/sum-of-beauty-of-all-substrings.java
+++ Sum of Beauty of All Substrings - GFG/sum-of-beauty-of-all-substrings.java
@@ -25,43 +25,4 @@ class GFG {
 
         
 
-class Solution {
-    public static int beautySum(String s) {
-        // code here
-        int n= s.length();
-        int ans = 0;
-        for(int i=3; i<=n; i++){
-            int l=0, j= i;
-            int[] freq= new int[26];
-            for(int k=l; k<j; k++){
-                freq[s.charAt(k)-'a']++;
-            }
-            
-            ans += beauty(freq);
-            
-            while(j<n){
-                freq[s.charAt(j)-'a']++;
-                freq[s.charAt(l)-'a']--;
-                ans += beauty(freq);
-                l++; 
-                j++;
-            }
-            
-            
-            
-        }
-        
-        return ans;
-    }
-    
-    static int beauty(int[] freq){
-        int mx=0, mn= Integer.MAX_VALUE;
-        for(int x: freq){
-            // if(x!=0){
-            mx= Math.max(mx, x);
-            mn= Math.min(mn, x);
-            // }
-        }
-        return mx-mn;
-    }
-}
+
