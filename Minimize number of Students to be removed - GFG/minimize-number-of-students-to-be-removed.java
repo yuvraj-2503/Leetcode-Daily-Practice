171c19730808d78dfe1b02b7e2a2e39cf8043933
Yuvraj Singh <singh.yuvraj1047@gmail.com>
2023-01-03 23:43:03 +0530

    Added solution - LeetHub

---
+++ Minimize number of Students to be removed - GFG/minimize-number-of-students-to-be-removed.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            
+            String S[] = read.readLine().split(" ");
+            int[] H = new int[N];
+            
+            for(int i=0; i<N; i++)
+                H[i] = Integer.parseInt(S[i]);
+
+            Solution ob = new Solution();
+            out.println(ob.removeStudents(H,N));
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public int removeStudents(int[] h, int n) {
+        // code here
+        ArrayList<Integer> dp=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            int idx=binarySearch(h[i],dp);
+            if(idx==dp.size()){
+                dp.add(h[i]);
+            }else{
+                dp.set(idx,h[i]);
+            }
+        }
+        return n-dp.size();
+    }
+    
+    int binarySearch(int ele,ArrayList<Integer> arr){
+        int l=0;
+        int h=arr.size()-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(arr.get(mid)>=ele){
+                h=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return l;
+    }
+};
